93e79a8de596bfb7f5d75b7d5e2e4cd1f4b78129
Russ Blackburn <russ.c.blackburn@gmail.com>
2015-10-13 13:04:11 -0600

    added the site map

---
+++ siteMap.php
@@ -0,0 +1,86 @@
+<?php $page = siteMap; ?>
+<?php require_once('header.php');?>
+
+<div class="pagePadding">
+<h1>Site Map</h1>
+<hr>
+
+    <div class="row">
+    
+        <!-- home -->
+        <div class="col-xs-12 col-sm-4 col-md-4">
+        <h3 class="thinText">Home</h3>
+        <hr>
+        <p><a href="index.php">Home</a></p>
+        </div>
+        <!-- end of home -->
+        
+        <!-- about -->
+        <div class="col-xs-12 col-sm-4 col-md-4">
+        <h3 class="thinText">About</h3>
+        <hr>
+        <p><a href="purpose.php">Purpose</a></p>
+        <p><a href="personnel.php">Personnel</a></p>
+        </div>
+        <!-- end of about -->
+        
+        <!-- services -->
+        <div class="col-xs-12 col-sm-4 col-md-4">
+        <h3 class="thinText">Services</h3>
+        <hr>
+        <p><a href="training.php">Training</a></p>
+        <p><a href="exercises.php">Exercises</a></p>
+        <p><a href="assessment.php">Assessment</a></p>
+        <p><a href="research.php">Research</a></p>
+        </div>
+        <!-- end of services -->
+        
+        <!-- events -->
+        <div class="col-xs-12 col-sm-4 col-md-4">
+        <h3 class="thinText">Events</h3>
+        <hr>
+        <p><a href="events.php">Events</a></p>
+        </div>
+        <!-- end of events -->
+        
+        <!-- gallery -->
+        <div class="col-xs-12 col-sm-4 col-md-4">
+        <h3 class="thinText">Gallery</h3>
+        <hr>
+        <p><a href="photos.php">Photos</a></p>
+        <p><a href="videos.php">Videos</a></p>
+        <p><a href="virtualTour.php">Virtual Tour</a></p>
+        </div>
+        <!-- end of gallery -->
+        
+        <!-- register -->
+        <div class="col-xs-12 col-sm-4 col-md-4">
+        <h3 class="thinText">Register</h3>
+        <hr>
+        <p><a href="studentRegistration.php">Student Registration</a></p>
+        <p><a href="instructorRegistration.php">Instructor Registration</a></p>
+        <p><a href="requestServiceProposal.php">Request Service Proposal</a></p>
+        </div>
+        <!-- end of register -->
+        
+        <!-- contact -->
+        <div class="col-xs-12 col-sm-4 col-md-4">
+        <h3 class="thinText">Contact</h3>
+        <hr>
+        <p><a href="contact.php">Contact</a></p>
+        </div>
+        <!-- end of contact -->
+        
+        <!-- faq -->
+        <div class="col-xs-12 col-sm-4 col-md-4">
+        <h3 class="thinText">FAQ</h3>
+        <hr>
+        <p><a href="faq.php">FAQ</a></p>
+        </div>
+        <!-- end of faq -->
+    
+    </div><!-- end of row -->
+
+
+</div><!-- end of pagePadding -->
+<?php require_once('footer.php'); ?>
