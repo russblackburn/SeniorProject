78ba851d2c3ea2a29f84360392dc59e3f29074dd
Jacob Faulkner <jacob.t.faulkner@gmail.com>
2015-10-28 16:54:51 -0600

    Updated admin landing

--- adminLanding.php
+++ adminLanding.php
@@ -2,8 +2,11 @@
 <?php $page = admin; ?>
 <?php require_once('header.php'); ?>
 
-
-<p>Add directions here</p>
+<h1>Welcome to the Admin Page</h1>
+<hr>
+<br>
+<p>Hover over form titles to get instructions for each form input.</p>
+<figure class="gifborder col-xs-12 col-sm-8 col-md-6 col-lg-5"><image src="images/admin/hoverInstructions.gif" alt="hover insturctions"></figure>
 
 
 <?php require_once('footer.php'); ?>
