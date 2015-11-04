4c2d729d31f97b9e736f54ca38c37481ba00a666
Russ Blackburn <russ.c.blackburn@gmail.com>
2015-11-04 14:05:02 -0700

    finished forms

--- requestServiceProposalForm.php
+++ requestServiceProposalForm.php
@@ -2,20 +2,10 @@
     
     <form action="requestServiceProposalProcess.php" method="POST" enctype="multipart/form-data" name="requestServiceProposal_registration">
     
-    <div class="form-group">
-    <label for="name">Name</label>
-    <input type="text" class="form-control" id="name" name="name" placeholder="Name" required>
-  	</div>
-  
-	<div class="form-group">
-    <label for="email">Email</label>
-    <input type="email" class="form-control" id="email" name="email" placeholder="Email" required>
-	</div>
-    
     <?php
 	
 	if($secondaryPage == requestServiceProposal){
-    echo '<label for="course">Service</label>';
+    echo '<label for="course">Service Requested</label>';
 				echo '<div class="form-group">';
 					echo '<div class="form-group">';
 						echo '<select class="form-control" name="service">';
@@ -37,6 +27,31 @@
 					echo '<input type="hidden" name="service" value="Research">';
 				}
 	?>
+    
+    <div class="form-group">
+    <label for="initiator">Initiator</label>
+    <input type="text" class="form-control" id="initiator" name="initiator" placeholder="Initiator" required>
+  	</div>
+  
+	<div class="form-group">
+    <label for="organizationDepartment">Organization/Department</label>
+    <input type="organizationDepartment" class="form-control" id="organizationDepartment" name="organization_department" placeholder="Organization/Department" required>
+	</div>
+    
+    <div class="form-group">
+    <label for="contactInformation">Contact Information</label>
+    <input type="contactInformation" class="form-control" id="contactInformation" name="contact_information" placeholder="Contact Information" required>
+	</div>
+    
+    <div class="form-group">
+    <label for="details">Details</label>
+    <textarea class="form-control" rows="3" name="details" placeholder="Details"></textarea>
+	</div>
+    
+    <div class="form-group">
+    <label for="serviceDeadline">Service Deadline</label>
+    <input type="contactInformation" class="form-control" id="serviceDeadline" name="service_deadline" placeholder="Service Deadline" required>
+	</div>
     
     <button type="submit" class="btn btn-default" name="submitButton">Register</button>
     
--- requestServiceProposalProcess.php
+++ requestServiceProposalProcess.php
@@ -6,9 +6,12 @@ require_once('header.php');
 $dbc1 = mysqli_connect(HOST1,USER1,PASSWORD1,DATABASE1) or die('The database connection has failed!');
 
 // get all of the elements from the form
-$name = stripslashes(mysqli_real_escape_string($dbc1, trim($_POST[name])));//comes through as the full name
-$email = stripslashes(mysqli_real_escape_string($dbc1, trim($_POST[email])));//comes through as email
 $service = stripslashes(mysqli_real_escape_string($dbc1, trim($_POST[service])));
+$initiator = stripslashes(mysqli_real_escape_string($dbc1, trim($_POST[initiator])));
+$organization_department = stripslashes(mysqli_real_escape_string($dbc1, trim($_POST[organization_department])));
+$contact_information = stripslashes(mysqli_real_escape_string($dbc1, trim($_POST[contact_information])));
+$details = stripslashes(mysqli_real_escape_string($dbc1, trim($_POST[details])));
+$service_deadline = stripslashes(mysqli_real_escape_string($dbc1, trim($_POST[service_deadline])));
 
 
 //SET THE DATE THAT THE USER HAS REGISTERED ON -----------
@@ -16,18 +19,18 @@ $service = stripslashes(mysqli_real_escape_string($dbc1, trim($_POST[service])))
 
 if($service == 'Exercises'){
 		// build the query
-		$query1 = "INSERT INTO exercises(name, email, registered_on)". 
-		"VALUES ('$name','$email','$registered_on')";
+		$query1 = "INSERT INTO exercises(initiator, organization_department, contact_information, details, service_deadline, registered_on)". 
+		"VALUES ('$initiator','$organization_department','$contact_information','$details','$service_deadline','$registered_on')";
 	}
 if($service == 'Assessment'){
 		// build the query
-		$query1 = "INSERT INTO assessment(name, email, registered_on)". 
-		"VALUES ('$name','$email','$registered_on')";
+		$query1 = "INSERT INTO assessment(initiator, organization_department, contact_information, details, service_deadline, registered_on)". 
+		"VALUES ('$initiator','$organization_department','$contact_information','$details','$service_deadline','$registered_on')";
 	}
 if($service == 'Research'){
 		// build the query
-		$query1 = "INSERT INTO research(name, email, registered_on)". 
-		"VALUES ('$name','$email','$registered_on')";
+		$query1 = "INSERT INTO research(initiator, organization_department, contact_information, details, service_deadline, registered_on)". 
+		"VALUES ('$initiator','$organization_department','$contact_information','$details','$service_deadline','$registered_on')";
 	}
 
 	// communicate the query with the database // SAVE THE FORM ELEMENTS INTO THE FORMS DATABASE
