3525ec060a561c458f01711fbadf76802f7ec1bf
Russ Blackburn <russ.c.blackburn@gmail.com>
2015-11-30 12:14:30 -0700

    uploaded help guide

--- adminLanding.php
+++ adminLanding.php
@@ -8,5 +8,11 @@
 <p>Hover over titles to get instructions for each form input.</p>
 <figure class="gifborder col-xs-12 col-sm-8 col-md-6 col-lg-5"><image src="images/admin/hoverInstructions.gif" alt="hover insturctions"></figure>
 
+<div class="clear"></div>
+
+<br><br>
+
+<p><a href="images/zipInstructions/help_guide.pdf" target="_blank">Help Guide Booklet</a></p>
+
 
 <?php require_once('footer.php'); ?>
